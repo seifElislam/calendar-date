7e1907f630c395091b7b55266e6a81956060b8ee
seifElislam <seif.eleslam1990@gmail.com>
2022-01-15 12:53:36 +0200

    Return today's gregorian representation

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 """
 APP module
 """
+from datetime import datetime
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, status
+from fastapi.responses import JSONResponse
 
 app = FastAPI()
 
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    today = datetime.today()
+    content = {"today": f'{today.strftime("%A, %d. %B %Y %I:%M%p")}'}
+    return JSONResponse(status_code=status.HTTP_200_OK, content=content)
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
