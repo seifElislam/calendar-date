088e9b2c94dc5ace8b11ee6f54f0febc7fc47fb2
seifElislam <seif.eleslam1990@gmail.com>
2022-01-15 14:58:05 +0200

    Create objects from available calendars then return today representation

--- main.py
+++ main.py
@@ -5,15 +5,19 @@ from datetime import datetime
 import uvicorn
 from fastapi import FastAPI, status
 from fastapi.responses import JSONResponse
+import calendars
 
 app = FastAPI()
 
 
 @app.get("/")
-async def root():
-    today = datetime.today()
-    content = {"today": f'{today.strftime("%A, %d. %B %Y %I:%M%p")}'}
-    return JSONResponse(status_code=status.HTTP_200_OK, content=content)
+async def today(languages=('en',)):
+    date = datetime.today()
+    response = {}
+    for cls in calendars.order:
+        calender = cls(languages)
+        response.update({calender.name :calender.get_today_representation(date)})
+    return JSONResponse(status_code=status.HTTP_200_OK, content=response)
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
